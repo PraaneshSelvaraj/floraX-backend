42876d8d0f0492c82a1ee8cfd4293da01b7328fa
Praanesh S <praaneshselvaraj2003@gmail.com>
2024-10-15 10:34:47 +0530

    Updated

--- includes/keys.py
+++ includes/keys.py
@@ -1,3 +1,1 @@
-mongo = "mongodb+srv://pottiflorax:rg5MiY4Fmp0Z2BxW@florax.vgqpqiu.mongodb.net/?retryWrites=true&w=majority"
-openweathermap = '077c227de8d61547f89391de738e5697'
-plant_id = 'JJ7ij5UUSbIZ5GcAzbGpuaB8NdOgAcdcxQa14AyDipb4GcPpuO'
+
